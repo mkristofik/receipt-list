078061be865cdd6d666804c2aa357c55ec545519
Michael Kristofik <kristo605@gmail.com>
2012-04-14 20:12:31 -0500

    Fix Bank of America parsing...again.

--- money.bat
+++ money.bat
@@ -1,4 +1,4 @@
 @echo off
-grep -P "\d\d-" %1 | grep " [-|+] " | python money.py - > money.html
+grep -P "\d\d-" %1 | grep "[-|+] " | python money.py - > money.html
 chrome.exe file:///%cd%/money.html
 del /p %1
