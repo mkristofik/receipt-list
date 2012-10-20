3d3eae8a5ae3699f0578fdc449d7a8ea4eb15120
Michael Kristofik <kristo605@gmail.com>
2012-10-19 20:58:13 -0500

    Change comments in batch files so they don't print.

--- money.bat
+++ money.bat
@@ -1,7 +1,7 @@
-rem          Copyright Michael Kristofik 2011-2012.
-rem Distributed under the Boost Software License, Version 1.0.
-rem    (See accompanying file LICENSE_1_0.txt or copy at
-rem          http://www.boost.org/LICENSE_1_0.txt)
+::           Copyright Michael Kristofik 2011-2012.
+::  Distributed under the Boost Software License, Version 1.0.
+::     (See accompanying file LICENSE_1_0.txt or copy at
+::           http://www.boost.org/LICENSE_1_0.txt)
 
 @echo off
 grep -P "\d\d-" %1 | grep "[-|+] " | python money.py - > money.html
