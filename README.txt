7a27f282c89d7f8f5422e51b1dcb7e458c000d03
Michael Kristofik <kristo605@gmail.com>
2013-02-23 22:44:31 -0600

    Update copyright for 2013.

--- README.txt
+++ README.txt
@@ -34,7 +34,7 @@ money eStmt_MM_DD_YYYY.txt
 
 LICENSE
 
-Copyright Michael Kristofik 2011-2012.
+Copyright Michael Kristofik 2011-2013.
 Distributed under the Boost Software License, Version 1.0.
 (See accompanying file LICENSE_1_0.txt or copy at 
 http://www.boost.org/LICENSE_1_0.txt)
--- money.py
+++ money.py
@@ -1,9 +1,9 @@
-#          Copyright Michael Kristofik 2011-2012.
+#          Copyright Michael Kristofik 2011-2013.
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
 
-""" Parser for a Bank of America statement. """
+""" Parser for a Bank of America text file statement (obsolete). """
 
 import checklist
 import fileinput
